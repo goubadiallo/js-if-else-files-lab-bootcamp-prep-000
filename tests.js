70f30f53a979cabb846a2a2854b815ccd69b8464
Mamadou Diallo <mamadou.diallo@wework.com>
2018-09-12 21:26:57 +0000

    automatically backed up by learn

--- tests.js
+++ tests.js
@@ -3,10 +3,6 @@ var index = require("./index.js")
 if (index.name === "Susan") {
     console.log("The name is correct")
 } 
-    
-}
-
-
 
 if (index.height === 70) {
     console.log("The height is correct")
