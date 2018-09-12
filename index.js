76d3c5a6a1a6d81dee04b188353f426360f68f3b
Mamadou Diallo <mamadou.diallo@wework.com>
2018-09-12 21:49:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
-var  name = 'Susan'
-if (name === "Susan"){
-  console.log("The name is correct")
-} else {
-  console.log("Expected the name to be Susan")
-}
+var  name = 'Joe'
+
 var  height = "74"
-if (height === "74"){
-  console.log("The height is correct")
-}
+
+
 
 
 
